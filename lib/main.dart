8ba2d95a0b737b7715d08c8e891b1c8b737f5ab9
BhavikJain07 <bhavikjainhp@gmail.com>
2025-03-25 10:47:11 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
     );
   }
 }
